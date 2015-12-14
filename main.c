19069ae1818e178872f2228edb0c68c0f1ee5faf
cesar <cesalberca@hotmail.es>
2015-12-14 21:12:09 +0100

    Dhondt

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 FILE *txtFile;
@@ -6,21 +7,25 @@ FILE *txtFile;
 struct Provincia {
   char nombre[100];
   int diputados;
-  int votosPP = 0;
-  int votosPsoe = 0;
-  int votosIu = 0;
-  int votosUpyd = 0;
-  int votosPodemos = 0;
-  int votosCiudadanos = 0;
-};
-
-struct Partido {
-  char nombre[100];
+  char votosPP[100];
+  char votosPsoe[100];
+  char votosIu[100];
+  char votosUpyd[100];
+  char votosPodemos[100];
+  char votosCiudadanos[100];
+  int diputadosPP;
+  int diputadosPsoe;
+  int diputadosIu;
+  int diputadosUpyd;
+  int diputadosPodemos;
+  int diputadosCiudadanos;
 };
 
 struct Provincia provincias[51];
 
+void inicializacionDiputados();
 void importTxt();
+void dhondt();
 
 int main(int argc, char *argv[]) {
 
@@ -128,12 +133,47 @@ int main(int argc, char *argv[]) {
   provincias[49] = ceuta;
   provincias[50] = melilla;
 
+  inicializacionDiputados();
   importTxt();
+  dhondt();
+
+
+  puts(provincias[0].nombre);
+  puts(provincias[0].votosPP);
+  puts(provincias[0].votosPsoe);
+
+  puts(provincias[22].nombre);
+  puts(provincias[22].votosPP);
+  puts(provincias[22].votosPsoe);
+}
+
+void inicializacionDiputados() {
+  int i;
+  for (i = 0; i < 51; i++) {
+    provincias[i].diputadosPP = 0;
+    provincias[i].diputadosPsoe = 0;
+    provincias[i].diputadosIu = 0;
+    provincias[i].diputadosUpyd = 0;
+    provincias[i].diputadosPodemos = 0;
+    provincias[i].diputadosCiudadanos = 0;
+  }
 }
 
 void importTxt() {
   char fileName[200];
-  char line[300];
+  char line1[100];
+  char line2[100];
+  char line3[100];
+  char line4[100];
+  char line5[100];
+  char line6[100];
+  char line7[100];
+  char line8[100];
+  char line9[100];
+  char line10[100];
+  char line11[100];
+  char line12[100];
+  char line13[100];
   int eofReturn = 0;
 
   puts("Introduce el nombre del archivo txt a importar:");
@@ -143,11 +183,66 @@ void importTxt() {
   txtFile = fopen(fileName, "r");
 
   do {
-    eofReturn = fscanf(txtFile, "%s", line);
-    if ((strcmp(line, "#") != 0)) {
-      printf("Provincia %s\n", line);
+    eofReturn = fscanf(txtFile, "%s", line1);
+    fscanf(txtFile, "%s", line2);
+    fscanf(txtFile, "%s", line3);
+    fscanf(txtFile, "%s", line4);
+    fscanf(txtFile, "%s", line5);
+    fscanf(txtFile, "%s", line6);
+    fscanf(txtFile, "%s", line7);
+    fscanf(txtFile, "%s", line8);
+    fscanf(txtFile, "%s", line9);
+    fscanf(txtFile, "%s", line10);
+    fscanf(txtFile, "%s", line11);
+    fscanf(txtFile, "%s", line12);
+    fscanf(txtFile, "%s", line13);
+
+    int i;
+    for (i = 0; i < 51; i++) {
+      if (strcmp(line1, provincias[i].nombre) == 0) {
+        strcpy(provincias[i].votosPP, line3);
+        strcpy(provincias[i].votosPsoe, line5);
+        strcpy(provincias[i].votosIu, line7);
+        strcpy(provincias[i].votosUpyd, line9);
+        strcpy(provincias[i].votosPodemos, line11);
+        strcpy(provincias[i].votosCiudadanos, line13);
+      }
     }
   } while((eofReturn != EOF));
 
   fclose(txtFile);
+}
+
+void dhondt() {
+    char* arrayNombres[] = {"PP", "PSOE", "IU", "UPYD", "Podemos", "Ciudadanos"};
+  long arrayVotos[6];
+  long temp1;
+  char* temp2;
+
+  int i;
+  for (i = 0; i < 51; i++) {
+    arrayVotos[0] = atol(provincias[i].votosPP);
+    arrayVotos[1] = atol(provincias[i].votosPsoe);
+    arrayVotos[2] = atol(provincias[i].votosIu);
+    arrayVotos[3] = atol(provincias[i].votosUpyd);
+    arrayVotos[4] = atol(provincias[i].votosPodemos);
+    arrayVotos[5] = atol(provincias[i].votosCiudadanos);
+
+    int j;
+    for (j = 0; j < 6; j++) {
+      int k;
+      for (k = 1 + j; k < 6; k++) {
+        if (arrayVotos[j] < arrayVotos[k]) {
+          temp1 = arrayVotos;
+          strcpy(temp2, arrayNombres);
+
+          arrayVotos[k] = arrayVotos[j];
+          strcpy(arrayNombres[j], arrayNombres[k]);
+
+          arrayVotos[j] = temp1;
+          strcpy(arrayNombres[j], temp2);
+        }
+      }
+    }
+  }
 }
